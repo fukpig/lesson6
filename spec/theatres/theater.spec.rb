7c51e1ae42d515fb33a445d8aee01e6bdf1441cd
fukpig <fukpig2@gmail.com>
2018-03-11 04:30:26 +0600

    fix therater specs

--- spec/theatres/theater.spec.rb
+++ spec/theatres/theater.spec.rb
@@ -23,60 +23,52 @@ describe Theater do
     let(:current_time) { Time.now.strftime("%H:%M") }
 
     context "check morning classic movie" do
-      let (:movie) { double("ClassicMovie") }
+      let (:movie) { double("ClassicMovie", :duration => 100, :title => "The thing") }
       it 'show film from morning period' do
-        movie.stub(:duration) { 100 }
-        movie.stub(:title) { "The thing" }
         theater.stub(:filter) { [movie] }
-
         movie_end_time = (Time.now + movie.duration*60).strftime("%H:%M")
         expect(theater.show("10:00")).to eq "Now showing: The thing #{current_time} - #{movie_end_time}"
       end
     end
 
     context "check day adventure or comedy movie" do
-    let (:movie) { double("ModernMovie") }
-      it 'get adventure film' do
-        movie.stub(:duration) { 110 }
-        movie.stub(:title) { "Indiana Jones" }
-        movie.stub(:genre) { ["Adventure"]}
-        theater.stub(:filter) { [movie] }
-
-        movie_end_time = (Time.now + movie.duration*60).strftime("%H:%M")
-        expect(theater.show("13:00")).to eq "Now showing: Indiana Jones #{current_time} - #{movie_end_time}"
+      context "adventure movie" do
+        let (:movie) { double("ModernMovie", :duration => 110, :title => "Indiana Jones", :genre => ["Adventure"]) }
+          it 'when adventure film' do
+            theater.stub(:filter) { [movie] }
+            movie_end_time = (Time.now + movie.duration*60).strftime("%H:%M")
+            expect(theater.show("13:00")).to eq "Now showing: Indiana Jones #{current_time} - #{movie_end_time}"
+          end
       end
 
-      it 'get comedy film' do
-        movie.stub(:duration) { 110 }
-        movie.stub(:title) { "Dumb and dumber" }
-        movie.stub(:genre) { ["Comedy"]}
-        theater.stub(:filter) { [movie] }
-
-        movie_end_time = (Time.now + movie.duration*60).strftime("%H:%M")
-        expect(theater.show("13:00")).to eq "Now showing: Dumb and dumber #{current_time} - #{movie_end_time}"
+      context "comedy movie" do
+        let (:movie) { double("ModernMovie", :duration => 110, :title => "Dumb and dumber", :genre => ["Comedy"]) }
+          it 'when adventure film' do
+            theater.stub(:filter) { [movie] }
+            movie_end_time = (Time.now + movie.duration*60).strftime("%H:%M")
+            expect(theater.show("13:00")).to eq "Now showing: Dumb and dumber #{current_time} - #{movie_end_time}"
+          end
       end
     end
 
-    context "check day horror or drama movie" do
-    let (:movie) { double("ModernMovie") }
-      it 'get horror film' do
-        movie.stub(:duration) { 110 }
-        movie.stub(:title) { "Alien" }
-        movie.stub(:genre) { ["Horror"]}
-        theater.stub(:filter) { [movie] }
 
-        movie_end_time = (Time.now + movie.duration*60).strftime("%H:%M")
-        expect(theater.show("21:00")).to eq "Now showing: Alien #{current_time} - #{movie_end_time}"
+    context "check evening horror or drama movie" do
+      context "adventure movie" do
+        let (:movie) { double("ModernMovie", :duration => 110, :title => "Alien", :genre => ["Horror"]) }
+          it 'when horror film' do
+            theater.stub(:filter) { [movie] }
+            movie_end_time = (Time.now + movie.duration*60).strftime("%H:%M")
+            expect(theater.show("21:00")).to eq "Now showing: Alien #{current_time} - #{movie_end_time}"
+          end
       end
 
-      it 'get drama film' do
-        movie.stub(:duration) { 110 }
-        movie.stub(:title) { "Bridgit jones" }
-        movie.stub(:genre) { ["Drama"]}
-        theater.stub(:filter) { [movie] }
-
-        movie_end_time = (Time.now + movie.duration*60).strftime("%H:%M")
-        expect(theater.show("21:00")).to eq "Now showing: Bridgit jones #{current_time} - #{movie_end_time}"
+      context "drama movie" do
+        let (:movie) { double("ModernMovie", :duration => 110, :title => "Bridgit jones", :genre => ["Drama"]) }
+          it 'when drama film' do
+            theater.stub(:filter) { [movie] }
+            movie_end_time = (Time.now + movie.duration*60).strftime("%H:%M")
+            expect(theater.show("21:00")).to eq "Now showing: Bridgit jones #{current_time} - #{movie_end_time}"
+          end
       end
     end
 
@@ -85,25 +77,5 @@ describe Theater do
         expect{theater.show("07:00")}.to raise_error(Theater::InvalidTimePeriod)
       end
     end
-
-=begin
-    it 'get film by morning period' do
-      expect(theater.show("10:00")).to eq "Now showing: #{morning_movie.title} #{current_time} - #{morning_movie_end_time}"
-    end
-
-    #array
-    it 'get film by day period' do
-      expect(theater.show("13:00")).to include("Now showing:")
-    end
-
-    #array
-    it 'get film by evening period' do
-      expect(theater.show("21:00")).to include("Now showing:")
-    end
-
-    it "get error by invalid period" do
-      expect{theater.show("07:00")}.to raise_error(Theater::InvalidTimePeriod)
-    end
-=end
   end
 end
